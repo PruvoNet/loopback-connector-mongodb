98293d0731bc2149b90a7b2ae92a688f519f1a57
Ruben Nogueira <ruben@pruvo.com>
2024-01-04 18:34:02 +0000

    feat: add tunnel ssh support

--- leak-detection/leak-detector.test.js
+++ leak-detection/leak-detector.test.js
@@ -3,7 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-var memwatch = require('memwatch-next');
+var memwatch = require('@airbnb/node-memwatch');
 
 describe('leak detector', function() {
   before(function() {
